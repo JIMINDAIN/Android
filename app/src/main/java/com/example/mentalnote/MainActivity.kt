75f1ca742af74a500a26db8b2cf406d604cf0037
dain00519 <ekdls030519@naver.com>
2025-07-07 11:07:40 +0900

    달력 상세 페이지 구현

--- app/src/main/java/com/example/mentalnote/MainActivity.kt
+++ app/src/main/java/com/example/mentalnote/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.example.mentalnote
 
-import androidx.compose.ui.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.Color
 import com.example.mentalnote.model.DayRecord
-import com.example.mentalnote.ui.WeekTab
-import com.example.mentalnote.ui.GalleryTab
-import com.example.mentalnote.ui.Tab2Screen
 import com.example.mentalnote.ui.MonthTab
+import com.example.mentalnote.ui.Tab2Screen
+import com.example.mentalnote.ui.WeekTab
 import com.example.mentalnote.ui.theme.MentalNoteTheme
 
 class MainActivity : ComponentActivity() {
