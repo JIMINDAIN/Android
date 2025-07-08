321e84a7e89c55a9a73ee083c9dd6ebfb1f13c9d
dain00519 <ekdls030519@naver.com>
2025-07-09 04:56:53 +0900

    회원가입 창 디자인 수정

--- app/src/main/java/com/example/mentalnote/ui/RegistrationScreen.kt
+++ app/src/main/java/com/example/mentalnote/ui/RegistrationScreen.kt
@@ -1,38 +1,43 @@
 package com.example.mentalnote.ui
 
-import android.app.Activity
+import android.widget.Toast
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import android.widget.Toast
 import androidx.datastore.preferences.core.edit
 import com.example.mentalnote.IS_LOGGED_IN
-import com.example.mentalnote.R
 import com.example.mentalnote.USER_PASSWORD
 import com.example.mentalnote.USER_USERNAME
 import com.example.mentalnote.dataStore
 import com.example.mentalnote.ui.theme.CustomFontFamily
-import com.google.firebase.auth.ActionCodeSettings
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
-import android.util.Log
 
 @Composable
 fun RegistrationScreen(
@@ -53,8 +58,8 @@ fun RegistrationScreen(
     ) {
         Column(
             modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
+                .fillMaxSize(),
+                //.padding(16.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
@@ -63,19 +68,20 @@ fun RegistrationScreen(
                 fontFamily = CustomFontFamily,
                 fontSize = 32.sp,
                 fontWeight = FontWeight.ExtraBold,
-                color = colorResource(id = R.color.y2k_primary)
+                color = Color(0xFF333333)
             )
-            Spacer(modifier = Modifier.height(32.dp))
+            Spacer(modifier = Modifier.height(64.dp))
 
             TextField(
                 value = email,
                 onValueChange = { email = it },
                 label = { Text("이메일 주소") },
                 singleLine = true,
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier.fillMaxWidth()
+                    .padding(horizontal = 32.dp),
                 colors = TextFieldDefaults.colors(focusedContainerColor = Color.White, unfocusedContainerColor = Color.White)
             )
-            Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(10.dp))
 
             TextField(
                 value = password,
@@ -83,10 +89,11 @@ fun RegistrationScreen(
                 label = { Text("비밀번호") },
                 singleLine = true,
                 visualTransformation = PasswordVisualTransformation(),
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier.fillMaxWidth()
+                    .padding(horizontal = 32.dp),
                 colors = TextFieldDefaults.colors(focusedContainerColor = Color.White, unfocusedContainerColor = Color.White)
             )
-            Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(10.dp))
 
             TextField(
                 value = confirmPassword,
@@ -94,7 +101,8 @@ fun RegistrationScreen(
                 label = { Text("비밀번호 확인") },
                 singleLine = true,
                 visualTransformation = PasswordVisualTransformation(),
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier.fillMaxWidth()
+                    .padding(horizontal = 32.dp),
                 colors = TextFieldDefaults.colors(focusedContainerColor = Color.White, unfocusedContainerColor = Color.White)
             )
             Spacer(modifier = Modifier.height(32.dp))
@@ -126,16 +134,22 @@ fun RegistrationScreen(
                 },
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(56.dp)
-                    .border(2.dp, colorResource(id = R.color.y2k_border), CutCornerShape(12.dp)),
-                shape = CutCornerShape(12.dp),
-                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFEE500))
+                    .padding(horizontal = 32.dp)
+                    .height(52.dp),
+                //.border(2.dp, colorResource(id = R.color.y2k_border), CutCornerShape(12.dp)),
+                shape = RoundedCornerShape(16.dp),
+                elevation = ButtonDefaults.buttonElevation(
+                    defaultElevation = 3.dp,
+                    pressedElevation = 2.dp,
+                    focusedElevation = 3.dp
+                ),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFFF0F5))
             ) {
                 Text(
                     "회원가입 완료",
                     fontFamily = CustomFontFamily,
                     fontSize = 18.sp,
-                    fontWeight = FontWeight.Bold,
+                    fontWeight = FontWeight.Medium,
                     color = Color.Black
                 )
             }
@@ -145,16 +159,22 @@ fun RegistrationScreen(
                 onClick = onBackToLogin,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(56.dp)
-                    .border(2.dp, colorResource(id = R.color.y2k_border), CutCornerShape(12.dp)),
-                shape = CutCornerShape(12.dp),
-                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFADD8E6))
+                    .padding(horizontal = 32.dp)
+                    .height(52.dp),
+                //.border(2.dp, colorResource(id = R.color.y2k_border), CutCornerShape(12.dp)),
+                shape = RoundedCornerShape(16.dp),
+                elevation = ButtonDefaults.buttonElevation(
+                    defaultElevation = 3.dp,
+                    pressedElevation = 2.dp,
+                    focusedElevation = 3.dp
+                ),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFFFDF0))
             ) {
                 Text(
                     "로그인 화면으로 돌아가기",
                     fontFamily = CustomFontFamily,
                     fontSize = 18.sp,
-                    fontWeight = FontWeight.Bold,
+                    fontWeight = FontWeight.Medium,
                     color = Color.Black
                 )
             }
